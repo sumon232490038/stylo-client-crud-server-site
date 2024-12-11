177b9b335424513958a54f81975386555a674c4b
sumon232 <sumonmia4526@gmail.com>
2024-12-12 00:38:44 +0600

    Alhamdulillah successfully crup oparetions are working

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 const uri =
-  "mongodb+srv://sumonmia4526:nerniSMAshiO@cluster0.xhl2h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://sumonmia4526:ry7cI7qFcAijgVKk@cluster0.xhl2h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -17,9 +17,49 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+  const newDatabase = client.db("styloClientDB").collection("styloData");
   try {
-    app.get("/", (req, res) => {
-      res.send("sumon bangladesh");
+    app.post("/", async (req, res) => {
+      const users = req.body;
+      const query = await newDatabase.insertOne(users);
+      res.send(query);
+    });
+
+    app.get("/users", async (req, res) => {
+      const findusers = newDatabase.find();
+      const result = await findusers.toArray();
+      //   console.log(result);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await newDatabase.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const findit = { _id: new ObjectId(id) };
+      const result = await newDatabase.findOne(findit);
+      //   console.log(result);
+      res.send(result);
+    });
+
+    app.put("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: user.name,
+          email: user.email,
+        },
+      };
+      const result = await newDatabase.updateOne(filter, updateDoc, options);
+      res.send(result);
     });
   } finally {
   }
